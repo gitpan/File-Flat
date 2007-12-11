5f4edefbc6fe42c2102a2899441b04eb447727b4
Adam Kennedy <adamk@cpan.org>
2007-12-11 06:29:43 +0000

    Import of ADAMK/File-Flat-1.01.tar.gz from CPAN.
    gitpan-cpan-distribution: File-Flat
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         ADAMK/File-Flat-1.01.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/File/Flat.pm
+++ lib/File/Flat.pm
@@ -19,7 +19,7 @@ use prefork    'File::Remove';
 
 use vars qw{$VERSION $errstr %modes $AUTO_PRUNE};
 BEGIN {
-	$VERSION = '1.00';
+	$VERSION = '1.01';
 
 	# The main error string
 	$errstr  = '';
@@ -1128,7 +1128,7 @@ L<File::Spec>, L<http://ali.as/>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2002 - 2006 Adam Kennedy. All rights reserved.
+Copyright (c) 2002 - 2006 Adam Kennedy.
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
 
